01b6eed3b230f7b7bf000139c08d54c67936648a
AndreiSchupov <115794058+GreatNorec@users.noreply.github.com>
2023-03-05 11:54:00 +0500

    Add files via upload

---
+++ exersize_15/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Enter the number of the day of the week: ");
+string num = Console.ReadLine();
+int n = int.Parse(num);
+string[] M = { "No", "No", "No", "No", "No", "Yes", "Yes"};
+Console.WriteLine("Is it today day off? - " + M[n-1]);
